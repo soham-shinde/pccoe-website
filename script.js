d9b999a4d779703c1152f8e3009370bde94e0e92
sohamshinde700 <88761532+sohamshinde700@users.noreply.github.com>
2023-01-28 11:59:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
     // define variables
     var items = document.querySelectorAll(".timeline li");
     var items1 = document.querySelectorAll(".luncher-timeline li");
+    var items2 = document.querySelectorAll(".rocket");
   
     // check if an element is in viewport
     // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
@@ -25,7 +26,23 @@
         }
       }
     }
+    function callbackFunc2() {
+      for (var i = 0; i < items2.length; i++) {
+          var windowHeight = window.innerHeight;
+          var elementTop = items2[i].getBoundingClientRect().top;
+          var elementVisible = 150;
+      
+          if (elementTop < windowHeight - elementVisible) {
+            items2[i].classList.add("active");
+          } else {
+            items2[i].classList.remove("active");
+          }
+       
+      }
+    }
     function callbackFunc1() {
+       
+
       for (var i = 0; i < items1.length; i++) {
         if (isElementInViewport(items1[i])) {
           items1[i].classList.add("in-view");
@@ -34,6 +51,7 @@
     }
     function reveal() {
         var reveals = document.querySelectorAll(".reveal");
+
       
         for (var i = 0; i < reveals.length; i++) {
           var windowHeight = window.innerHeight;
@@ -46,6 +64,7 @@
             reveals[i].classList.remove("active");
           }
         }
+
       }
   
     // listen for events
@@ -55,6 +74,7 @@
     window.addEventListener("load", callbackFunc);
     window.addEventListener("resize", callbackFunc);
     window.addEventListener("scroll", callbackFunc);
+    window.addEventListener("scroll", callbackFunc2);
     window.addEventListener("scroll", reveal);
   })();
   
